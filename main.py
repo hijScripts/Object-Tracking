9dc978166f2e3a0874b994c85c04cdb5965d4964
Harrison <harrisonjenkins1902@gmail.com>
2024-10-08 22:51:10 +1030

    Added getParams func

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 import cvzone
 import cv2
 import numpy as np
+from typing import Tuple
 
 from ultralytics import YOLO
+from ultralytics.yolo.engine.results import Boxes
 
 # Weights for detecting objects
 objectModel = YOLO("yolo11n.pt")
+
+def getParams(object: Boxes) -> Tuple[str, float]:
+    """
+    Purpose:
+    
+    Desc:
+    
+    :param object: an object found by YOLO
+    :type object: Boxes
+    
+    :return: Returns the className and confidence value of the object
+    :rtype: Tuple[str, float]
+    
+    :raises ExceptionType: condition
+    
+    >>> name, confidence = getParams(object: Boxes) -> Tuple[str, float]
+    name = person, confidence = 87.234324234234%
+    """
+
+    # Getting num found by yolo and matching to associated name
+    classNum = int(object.cls[0])
+    className = objectModel.names[classNum]
+
+    # Getting confidence value and converting to %
+    confidence = float(object.conf[0])
+    confidence *= 100
+
+    return className, confidence
 
 
 def detectPerson(image):
