ea887518ca1557167449288c5dde2158a568cf61
Harrison <harrisonjenkins1902@gmail.com>
2024-10-08 22:51:38 +1030

    removed detectperson func

--- main.py
+++ main.py
@@ -37,32 +37,6 @@ def getParams(object: Boxes) -> Tuple[str, float]:
 
     return className, confidence
 
-
-def detectPerson(image):
-
-    # Finding all objects in frame
-    objects = objectModel(image)
-
-    # Going over objects
-    for object in objects:
-
-        parameters = object.boxes
-
-        for box in parameters:
-
-            # Getting class number and converting to string
-            classDetected = box.cls[0]
-            classDetected = int(classDetected)
-            classDetectedName = objects[0].names[classDetected]
-
-            # Only displaying persons
-            if classDetectedName == "person":
-                x1, y1, x2, y2 = box.xyxy[0].numpy().astype("int")
-                confidence = box.conf[0]
-
-                cv2.rectangle(image, (x1, y1), (x2, y2), (50, 50, 255), 3)
-                cvzone.putTextRect(image, f"{classDetectedName} | {confidence:.2f}% Confident", [x1 + 8, y1 - 12])
-
 def main():
 
     # Capturing live webcam video
